f31f76935c5296b3dcd2bb05c1fee13b3991fb43
Olivier Pisano <olivier.pisano@laposte.net>
2017-06-17 17:40:21 +0200

    Added copyright notice

--- source/app.d
+++ source/app.d
@@ -1,5 +1,5 @@
 /*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
+    Copyright 2017 Olivier Pisano.
     This file is part of Leek.
 
     Leek is free software: you can redistribute it and/or modify
